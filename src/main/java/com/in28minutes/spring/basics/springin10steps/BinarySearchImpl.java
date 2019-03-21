4bdc308c0afac7d547457e6a402fe1a389f6b232
mohamed_sabri <mohamed.sabri@visionvalley.net>
2019-03-21 12:46:01 +0200

    first commit

---
+++ src/main/java/com/in28minutes/spring/basics/springin10steps/BinarySearchImpl.java
@@ -0,0 +1,8 @@
+package com.in28minutes.spring.basics.springin10steps;
+
+public class BinarySearchImpl {
+
+	// Sorting an array
+	//Search the array
+	//Return the Result ( the index of the searched item)
+}
---
+++ src/main/java/com/in28minutes/spring/basics/springin10steps/SpringIn10StepsApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes.spring.basics.springin10steps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIn10StepsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIn10StepsApplication.class, args);
+	}
+
+}
